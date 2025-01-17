e10f75a7392c01936c137b4f591e1625f1a3f421
EmilMustafaev <emilmustafaev891@gmail.com>
2025-01-17 14:41:40 +0300

    GitHub аналитика [#505139]

--- src/main/java/ru/job4j/github/analysis/service/GitHubService.java
+++ src/main/java/ru/job4j/github/analysis/service/GitHubService.java
@@ -1,19 +1,30 @@
 package ru.job4j.github.analysis.service;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import ru.job4j.github.analysis.model.Commit;
 import ru.job4j.github.analysis.model.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class GitHubService {
 
+    @Autowired
+    private final RestTemplate restTemplate;
+
+
     public List<Repository> fetchRepositories(String username) {
-        return List.of();
+        String url = "https://api.github.com/users/" + username + "/repos";
+        return restTemplate.getForObject(url, List.class);
     }
 
-    public List<Commit> fetchCommits(String repository) {
-        return List.of();
+    public List<Commit> fetchCommits(String repoName) {
+        String url = "https://api.github.com/repos/" + repoName + "/commits";
+        return restTemplate.getForObject(url, List.class);
     }
 }
